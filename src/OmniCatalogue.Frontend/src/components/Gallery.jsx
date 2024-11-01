9c191b63b3df031826de66f8bad1f6a465b539b4
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2024-11-01 20:03:25 +0100

    image details modal

--- src/OmniCatalogue.Frontend/src/components/Gallery.jsx
+++ src/OmniCatalogue.Frontend/src/components/Gallery.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
 import { fetchGalleryImages } from '../services/api';
 import ImageGalleryCard from './ImageGalleryCard';
+import ImageDetailsModal from './ImageDetailsModal';
 
 const Gallery = () => {
   const [images, setImages] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     const loadImages = async () => {
@@ -13,6 +15,14 @@ const Gallery = () => {
     };
     loadImages();
   }, []);
+
+  const handleCardClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
 
   return (
     <Box
@@ -27,14 +37,23 @@ const Gallery = () => {
       {images.map((image) => (
         <Box
           key={image.imageUrl}
+          onClick={() => handleCardClick(image)}
           sx={{
             flex: '1 1 300px',
             maxWidth: '350px',
+            cursor: 'pointer',
           }}
         >
           <ImageGalleryCard image={image} />
         </Box>
       ))}
+
+      {selectedImage && (
+        <ImageDetailsModal
+          image={selectedImage}
+          onClose={handleCloseModal}
+        />
+      )}
     </Box>
   );
 };
